14ede22aedf8b031ab86d863b4af28d5b1237bab
unknown <reymonthatarigan13@gmail.com>
2025-06-17 12:01:23 +0700

    Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,6 @@
 FROM stain/jena-fuseki:latest
 
-# Salin folder TTL dataset
 COPY ./dataset /fuseki/configuration/
-
-# Salin skrip entrypoint
 COPY entrypoint.sh /app/entrypoint.sh
 
-# Jalankan dengan bash langsung agar tidak perlu chmod
-ENTRYPOINT ["bash", "/app/entrypoint.sh"]
+ENTRYPOINT ["sh", "/app/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-# Buat dataset dan load semua TTL
-for ttl in /fuseki/configuration/*.ttl; do
-    dataset=$(basename "$ttl" .ttl)
-    /jena-fuseki/fuseki-server --update --mem /${dataset} &
-    sleep 5
-    curl -X POST --data-binary "@$ttl" \
-         -H "Content-Type: text/turtle" \
-         http://localhost:3030/${dataset}/data
-done
-
-# Jalankan Fuseki
-exec /jena-fuseki/fuseki-server
+exec /fuseki/fuseki-server --config=/fuseki/configuration/config.ttl --port=3030
